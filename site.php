92634c9a85dfbc9820622de30612e65e3a39c6ef
MatheusHenriquedeSouza <48722541+Matheusdelicinha@users.noreply.github.com>
2019-04-09 11:50:27 -0300

    Add files via upload

---
+++ site.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+	<title>
+		Projeto App Minha Casa minha Vida
+	</title>
+	<link type="text/css" href="estilo.css" rel="stylesheet">
+	</head>
+<body>
+	<div class="topo">
+		<img src="1.jpg" alt="mostrar imagem" width=1370 height=100>
+	</div>
+	<div class="menu">
+		<ul>
+			<li><a href="site.php">Home</a></li>		
+			<li><a href="perfil.php">Perfil</a></li>		
+		   <li><a href="contato.php">Contato</a></li>		
+		</ul>
+	</div>
+	<div class="conteudo">
+		Conteúdo principal	
+	</div>
+	<div class="rodape">
+		Desenvolvido por Rafael A. Florindo<br>
+		<a href="mailto:rafael.florindo@escola.pr.gov.br">Contato</a>
+	</div>
+</body>
+</html>
